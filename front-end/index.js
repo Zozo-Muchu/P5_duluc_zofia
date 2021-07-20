84394c743d234eee01088cb5fe53bf255f995997
Zozo-Muchu <zofia.duluc@gmail.com>
2021-07-20 18:07:48 +0200

    java, prix qui change pour les ours, ajustement css formulaire des coordonnées

---
+++ front-end/index.js
@@ -0,0 +1,55 @@
+/*----------------prix des ours celon taille--------------------*/
+const oursPrix = document.querySelector(".ours_prix");
+const trente = document.getElementById("trente");
+const cinquante = document.getElementById("cinquante");
+const soixante = document.getElementById("soixante");
+const quatrevingt = document.getElementById("quatrevingt");
+
+trente.addEventListener("click", () => {
+  oursPrix.innerHTML = 25 + "€";
+});
+
+cinquante.addEventListener("click", () => {
+  oursPrix.innerHTML = 30 + "€";
+});
+
+soixante.addEventListener("click", () => {
+  oursPrix.innerHTML = 35 + "€";
+});
+
+quatrevingt.addEventListener("click", () => {
+  oursPrix.innerHTML = 45 + "€";
+});
+
+/*-----------------------fin prix des ours celon taille -----------*/
+
+/*------------------------tableau achats produits-------------------*/
+/*-----ESSAI echec-----
+
+function Products(photo, nom, prix) {
+  (this.photo = photo), (this.nom = nom), (this.prix = prix);
+}
+
+const product1 = new Products("image/teddy_1.jpg", "Ours brun", 20);
+const product2 = new Products("image/teddy_2.jpg", "Ours creme", 25);
+const product3 = new Products("image/teddy_3.jpg", "Ours duo", 40);
+const product4 = new Products("image/teddy_4.jpg", "Ours tee-shirt", 30);
+const product5 = new Products("image/teddy_5.jpg", "Ours doux", 30);
+
+let products = [];
+products.push(product1, product2, product3, product4, product5);
+
+function populateTableList() {
+  let listOfProducts = "";
+
+  products.forEach(
+    (prod) =>
+      (listOfProducts += `
+    <tr>
+      <td><img src=${prod.pic}></td>
+    `)
+  );
+}
+document.getElementById("productList").innerHTML = listOfProducts;
+
+--*/
