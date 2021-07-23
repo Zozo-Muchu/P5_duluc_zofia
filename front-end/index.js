7f94ff3ae91bbf4747172c7b1009d4f039e79c20
Zozo-Muchu <zofia.duluc@gmail.com>
2021-07-23 18:00:06 +0200

    essai appel nounours

--- front-end/index.js
+++ front-end/index.js
@@ -24,7 +24,7 @@ quatrevingt.addEventListener("click", () => {
 
 /*-----------------------fin prix des ours celon taille -----------*/
 
-/*------------------------tableau achats produits-------------------*/
+/*------------------------tableau achats produits du panier-------------------*/
 /*-----ESSAI echec-----
 
 function Products(photo, nom, prix) {
@@ -56,17 +56,63 @@ document.getElementById("productList").innerHTML = listOfProducts;
 --*/
 /*-----------------appel du back end dans acceuil------------------------*/
 let url = " http://localhost:3000/api/teddies";
+/*---lien vers le serveur-----*/
 let list_teddy = "";
 fetch(url).then((response) =>
   response.json().then((data) => {
+    /*----récupération des données---*/
     for (let teddy of data) {
-      list_teddy += `<li> <a class="button">
+      list_teddy += `<li><a href="oursbrun.html?_id=${teddy._id}" class="button">
         <img src="${teddy.imageUrl}"/>
         <h2>${teddy.name}</h2>
         <div>${teddy.description}"</div>
-        <div class="price">${teddy.price}€</div>`;
+        <div class="price">${teddy.price}€</div>
+        </a>`;
     }
     console.log(list_teddy);
     document.querySelector("#groupe_ours").innerHTML = list_teddy;
   })
 );
+/*---------------- ESSAI appel de la page produits------------*/
+
+/*récupération de l'id des produits
+const Parametre = new URLSearchParams(location.search);
+const newId = Parametre.get("_id");
+
+/*Recherche de l'adresse de l'id
+
+const newURL = `http://localhost:3000/api/teddies/${newId}`;
+
+fetch(newURL)
+  .then((response) => response.json())
+  .then((data) => {
+    const teddyPrdoduit = data;
+    addCard(data);
+
+    function addCard(teddyProduit) {
+
+      const teddyImage = document.getElementById("teddy_image");
+      teddyImage.innerHTML += `
+  <img src="${teddy.imageUrl}" alt="${teddy.name}">`;
+
+      const teddyName = document.getElementById("teddy_nom");
+      teddyName.innerHTML += `
+  <h3>${teddy.name}</h5>`;
+
+      const teddyDescription = document.getElementById("teddy_description");
+      teddyDescription.innerHTML += `
+  <p>${teddy.description}</p>`;
+
+      const teddyPrix = document.getElementById("teddy_prix");
+      teddyPrix.innerHTML += `
+  <h3>${convertPrice(teddy.price)}</h3>`;
+    }
+  });
+/*--créer un Ours--
+let TeddyObjet = new teddyPrdoduit(
+  newId,
+  teddy.imageUrl,
+  teddy.name,
+  teddy.description,
+  teddy.price
+);*/
