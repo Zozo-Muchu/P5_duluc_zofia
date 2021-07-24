3bd72a9059372564d8a3f492f528e713047ac0dd
Zozo-Muchu <zofia.duluc@gmail.com>
2021-07-24 18:00:23 +0200

    mettre en forme les prix, mise en place des options

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -62,11 +62,13 @@ fetch(url).then((response) =>
   response.json().then((data) => {
     /*----récupération des données---*/
     for (let teddy of data) {
-      list_teddy += `<li><a href="oursbrun.html?_id=${teddy._id}" class="button">
+      list_teddy += `<li><a href="oursbrun.html?_id=${
+        teddy._id
+      }" class="button">
         <img src="${teddy.imageUrl}"/>
         <h2>${teddy.name}</h2>
         <div>${teddy.description}"</div>
-        <div class="price">${teddy.price}€</div>
+        <div class="price">${teddy.price / 100}€</div>
         </a>`;
     }
     console.log(list_teddy);
--- front-end/js/ours.js
+++ front-end/js/ours.js
@@ -10,13 +10,27 @@ let newId = urlParametre.get("_id");
 
 let newURL = "http://localhost:3000/api/teddies/" + newId;
 
+/*switch option*/
+
 fetch(newURL).then((response) =>
   response.json().then((data) => {
+    let listcolor = "";
+    for (let color of data.colors) {
+      listcolor += `<option value="Sélectionner" class="Selectionner" style="color:${color}">${color}</option>`;
+    }
     carte_ours = `
     <img src=${data.imageUrl} class="ourson" alt="${data.name}">
     <div class="description_ours">
     <h2>${data.name}</h2>
-    <p id="prix_ours">${data.price}</p>
+    <p id="prix_ours">${data.price / 100}€</p>
+    <div class="colors">
+    <form method="post">
+    <p>
+    <label for="couleur">Choisissez la Couleur</label><br/>
+    <select name="couleur" id="couleur">
+    ${listcolor}
+    </select>
+    </form>
     <div class="button_ours">
     <input class="button_ajout" type="button" value="Ajouter">
     </div>
